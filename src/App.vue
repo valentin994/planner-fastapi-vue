531e179ae126e2844a5a73c9abe6a8edfef4abbe
valentin994 <valentin.vareskic@outlook.com>
2021-11-26 16:39:37 +0100

    added todos to main page, removed HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <h1>Ghost Planner</h1>
+  <TodoList :todos="todos" />
 </template>
 
 <script>
+import TodoList from "./components/TodoList.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { TodoList },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    async getTodos() {
+      try {
+        let response = await fetch("http://127.0.0.1:8000/todos/");
+        this.todos = await response.json();
+        console.log(this.todos);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getTodos();
+  },
 };
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul v-for="todo in todos" :key="todo.text">
+      <li>Title: {{ todo.title }}</li>
+      <li>Text: {{ todo.text }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ["todos"],
+};
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
