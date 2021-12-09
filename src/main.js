6f43b84e2a7b19bf228a183e968854af8293b4d2
valentin994 <valentin.vareskic@outlook.com>
2021-12-09 13:43:46 +0100

    initiated vuex to the project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import App from "./App.vue";
+import { createLogger } from "vuex";
+//TODO implement filter option
+//TODO move store to separate file
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  plugins: [createLogger()],
+});
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(store);
+app.mount("#app");
