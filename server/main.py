cb4161c2819c6b480baf9ea70ee5c2cbff61b3e8
valentin994 <valentin.vareskic@outlook.com>
2021-12-23 17:06:43 +0100

    moved req file, updated github action yaml

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
